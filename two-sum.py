37b3b9b8d93f6a4d2b26024f935e73ef20f42f17
Draconicus Malicalus <firstgcoders@gmail.com>
2021-09-30 13:45:42 -0700

    Update two-sum.py

--- two-sum.py
+++ two-sum.py
@@ -1,3 +1,6 @@
+# For this I took the approach to subtract from the target and see if the remainder is present in the list
+# This removes the need to check every pair and add them together
+
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         nums = []
